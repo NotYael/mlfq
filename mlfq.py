12c87770f8ee1d69f75b7994e12ccde485a2c83f
NotYael <danyaeldelacruz@gmail.com>
2024-12-18 12:17:01 +0800

    feat: added classes

--- mlfq.py
+++ mlfq.py
@@ -4,3 +4,27 @@
 # Kaila Ondoy
 # Sam Teng
 
+import math
+
+class Queue_1:
+    time_allotment = 8
+    queue = []
+
+class Queue_2:
+    time_allotment = 8
+    queue = []
+
+class Queue_3:
+    time_allotment = math.inf
+    queue = []
+
+class Process:
+    def __init__(self, arrival_time, burst_time: list, i_o: list):
+        self.arrival_time = arrival_time
+        self.burst_time = burst_time
+        self.i_o = i_o
+
+# Sample given in specs, but not sure if this is the proper way to input
+A = Process(0, [5, 5, 5], [2, 2])
+B = Process(2, [2, 6], [2])
+C = Process(0, [30])
