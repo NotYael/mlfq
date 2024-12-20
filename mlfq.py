4a2975b6e6584ac958b5463f9b4c245c8e671cd5
NotYael <danyaeldelacruz@gmail.com>
2024-12-20 09:57:46 +0800

    feat: replaced old mlfq py file with kaila's mlfq

--- mlfq.py
+++ mlfq.py
@@ -4,181 +4,222 @@
 # Kaila Ondoy
 # Sam Teng
 
-# TODO:
-# - Implement Round Robin
-# - Implement changing of priority
-# - Implement I/O time
-# - Implement context switching
-# - Implement sorting of processes in queue (if needed)
-# - Implement final results (turn around time, waiting time, etc.)
-
-import math
-
 class Queue:
-    def __init__(self, priority, time_quantum):
+    def __init__(self, priority: int, time_allotment: int):
         self.priority = priority
-        self.time_quantum = time_quantum
-        self.queue = []
-
-    def __str__(self):
-        return f"Queue(time_quantum={self.time_quantum}, self.queue={self.queue})"
+        self.time_allotment = time_allotment
+        self.processes = []
 
 class Process:
-    def __init__(self, label, arrival_time, cpu_time: list, io_time: list):
-        self.label = label
+    def __init__(self, name: str, arrival_time: int, cpu_bursts: list[int], io_bursts: list[int]):
+        self.name = name
         self.arrival_time = arrival_time
-        self.cpu_time = cpu_time
-        self.io_time = io_time
-        self.priority = 0
-        self.used_quantum = 0
-        self.current_burst = 0
-        self.current_io = 0
+        self.cpu_bursts = cpu_bursts
+        self.io_bursts = io_bursts
 
+        self.priority = 0
+        self.curr_cpu = 0
+        self.curr_io = 0
+        self.uptime = 0
+
+        self.completion_time = 0
+        self.total_burst = sum(cpu_bursts) + sum(io_bursts)
+    
     def __str__(self):
-        return f"Process(label={self.label}, arrival_time={self.arrival_time}, cpu_bursts={self.cpu_time}, io_bursts={self.io_time})"
+        return f"[{self.name} : {self.priority}]\t| UPTIME: {self.uptime}\t| CPU: {self.curr_cpu}\t| I/O: {self.curr_io}\t| CPU BURSTS: {self.cpu_bursts}\t| I/O BURSTS: {self.io_bursts}"
 
 def get_input():
     queues = []
     processes = []
 
     print("# Enter Scheduler Details #")
-    num_processes = int(input(""))
-    for i in range(2):
-        time_allotment = int(input())
-        queues.append(Queue(i, time_allotment))
-    # Infinite time allotment for last queue
-    queues.append(Queue(2, math.inf))
-
-    context_switch = int(input(""))
+    num_processes = int(input())
+    for i in range(2): queues.append(Queue(i, int(input())))
+    queues.append(Queue(2, 50))
+    context_switch = int(input())
 
     print(f"# Enter {num_processes} Process Details #")
     for i in range(num_processes):
         cpu_bursts = []
         io_bursts = []
-        input_data = input().split(";")
-        label = input_data[0]
-        arrival_time = int(input_data[1])
-        for i in range(2, len(input_data)):
-            if i % 2 == 0:
-                cpu_bursts.append(int(input_data[i]))
-            else:
-                io_bursts.append(int(input_data[i]))
-        
-        processes.append(Process(label, arrival_time, cpu_bursts, io_bursts))
+        user_input = input().split(";")
+        name = user_input[0]
+        arrival_time = int(user_input[1])
+        for i in range(2, len(user_input)):
+            if i % 2 == 0: cpu_bursts.append(int(user_input[i]))
+            else: io_bursts.append(int(user_input[i]))
+        processes.append(Process(name, arrival_time, cpu_bursts, io_bursts))
 
-    # for queue in queues:
-    #     print(queue)
+    return queues, processes, num_processes, context_switch
 
-    # for process in processes:
-    #     print(process)
+def enqueue(queues: list[Queue], processes: list[Process]):
+    if processes:
+        for queue in queues:
+            for process in processes:
+                if process.priority == queue.priority:
+                    queue.processes.append(process)
+                    # processes.remove(process)
 
-    return queues, processes, context_switch
-
-# MLFQ Algorithm
-# while there are still processes to run
-#  if there are processes that have arrived, insert them to the ready queue in order of label
-#  if the CPU is not running anything, get the next process to run
-#  if the CPU has used up its quantum, move the process to the next queue
-#  if the CPU has finished its burst, move the process to the next queue
-#  if the process has finished all its bursts, remove it from the system
-#  if the process has an IO burst, move it to the IO queue
-
-def mlfq(queues: list, processes: list, context_switch: int):
-
+def mlfq(queues: list[Queue], processes: list[Process], num_processes: int, context_switch: int):
     time = 0
-    num_processes = len(processes)
-    done_processes = 0
-    ready_queue = []
-    io_queue = []
-    cpu = None
+    curr_cs = 0
+
+    cpu: Process = None
+    io: list[Process] = []
+    
+    completed: list[Process] = []
 
     print("# Scheduling Results #")
 
-    while done_processes < num_processes:
+    while processes:
         
-        print(f"At Time = {time}")
+        from_new: list[Process] = []
+        from_cpu: list[Process] = []
+        from_io: list[Process] = []
 
-        # Case of arriving process
-        arrived = [process for process in processes if process.arrival_time == time]
-        if arrived:
-            arrived.sort(key=lambda Process: Process.label)
-            ready_queue += arrived
-            print(f"Arriving : [{', '.join(process.label for process in arrived)}]")
+        done = None
+        demotion = None
+
+        # [X] Remove completed process
+        # [X] Move process between CPU and I/O
+
+        # [X] Enqueue newly arriving processes
+        # [X] Enqueue process from CPU
+        # [X] Enqueue processes from I/O
+        # [X] Context switch to new process
+
+        for process in processes:
+            # Remove process if completed
+            if not (process.curr_cpu or process.curr_io or process.cpu_bursts or process.io_bursts):
+                done = process.name
+                process.completion_time = time
+                completed.append(process)
+                if process == cpu:
+                    cpu = None
+                    curr_cs = context_switch
+                elif process in io: io.remove(process)
+                processes.remove(process)
+                continue
+
+            # Handle process from new arrivals
+            if process.arrival_time == time:
+                from_new.append(process)
+            
+            # Handle process from CPU
+            if process == cpu:
+                # Process exceeds time allotment
+                if process.uptime == queues[process.priority].time_allotment:
+                    demotion = process.name
+                    process.priority += 1
+                    process.uptime = 0
+                    cpu = None
+                    curr_cs = context_switch
+                    from_cpu.append(process)
+                    if process.curr_cpu: process.cpu_bursts.insert(0, process.curr_cpu)
+
+                # Process completes RR quantum
+                elif process.priority == 0 and process.uptime == 4:
+                    cpu = None
+                    curr_cs = context_switch
+                    from_cpu.append(process)
+                    if process.curr_cpu: process.cpu_bursts.insert(0, process.curr_cpu)
+
+                # Process completes CPU burst
+                if process.curr_cpu == 0:
+                    if process.io_bursts:
+                        process.curr_io = process.io_bursts.pop(0)
+                        process.uptime = 0
+                        io.append(process)
+                    cpu = None
+                    curr_cs = context_switch
+
+            # Handle process from I/O
+            if process in io:
+                if process.curr_io == 0 and process.cpu_bursts:
+                    io.remove(process)
+                    from_io.append(process)
+                else: process.curr_io -= 1
         
-        # if ready queue not empty, insert processes to queues based on priority
-        if ready_queue:
-            for process in ready_queue:
+        # Enqueue newly arriving processes
+        enqueue(queues, sorted(from_new, key=lambda Process: Process.name))
+        # Enqueue process from CPU
+        enqueue(queues, sorted(from_cpu, key=lambda Process: Process.name))
+        # Enqueue processes from I/O
+        enqueue(queues, sorted(from_io, key=lambda Process: Process.name))
+
+        # Context switch to new process
+        if curr_cs > 0:
+            print(f"[DEBUG] CONTEXT SWITCH TIME LEFT: {curr_cs}")
+            curr_cs -= 1
+        else:
+            if not cpu:
                 for queue in queues:
-                    if process.priority == queue.priority:
-                        queue.queue.append(process)
-                        ready_queue.remove(process)
+                    if queue.processes:
+                        cpu = queue.processes.pop(0)
+                        cpu.curr_cpu = cpu.cpu_bursts.pop(0)
+                        break
 
+            if cpu:
+                cpu.curr_cpu -= 1
+                cpu.uptime += 1
+                
 
-        # case for when CPU has used up its quantum
-        if cpu and cpu.used_quantum == queues[cpu.priority].time_quantum:
-            pass
+        # [X] Output time
+        # [X] Output newly arriving processes
+        # [X] Output process done
+        # [X] Output queue states
+        # [X] Output process in CPU
+        # [X] Output processes in I/O
+        # [X] Output process demotion
 
-        # account for round robin somewhere 
-
-        # swap cpu if quantum is used up / and also check if there is still remaining burst and i/o time
-        if cpu and cpu.current_burst == 0:
-            pass
-
-        # if cpu is not running anything, get the next process to run.
-        if not cpu:
-            for queue in queues:
-                if queue.queue:
-                    # ADD SOME CHECKING HERE FOR CASE OF AFTER IO / GOES BACK INTO READY QUEUE / ETC.
-                    # what if theres is already an existing process in the queue and another process arrives? ([C], then B arrives, need cause to handle sorting WHEN NEEDED) 
-                    #   - still dk when this applies cause I already sorted in ready queue but I feel like this is important
-                    cpu = queue.queue.pop(0)
-                    # set the current burst to the first burst in the list
-                    cpu.current_burst = cpu.cpu_time.pop(0)
-                    break
-
-        queue_status = [f"[{' '.join(process.label for process in queue.queue)}]" for queue in queues]
-        print(f"Queues : {' '.join(queue_status)}")
-        print(f"CPU : {cpu.label}")
-        if io_queue:
-            print(f"IO : [{' '.join(process.label for process in io_queue)}]")
-        print("\n")
-
-        # add case for lowering priority 
-        # account for context switching
+        # [DEBUG]
+        for process in processes: print(process)
+        
+        # Output time
+        print(f"At Time = {time}")
+        # Output newly arriving processes
+        if from_new: print(f"Arriving : [{', '.join(process.name for process in from_new)}]")
+        # Output process done
+        if done: print(f"{done} DONE")
+        # Output queue states
+        queue_status = [f"[{', '.join(process.name for process in queue.processes)}]" for queue in queues]
+        print(f"Queues : {';'.join(queue_status)}")
+        # Output process in CPU
+        if cpu: print(f"CPU : {cpu.name}")
+        else: print("CPU : []")
+        # Output processes in I/O
+        if io: print(f"I/O : [{', '.join(process.name for process in io)}]")
+        # Output process demotion
+        if demotion: print(f"{demotion} DEMOTED")
+        print()
 
         time += 1
-        if cpu:
-            cpu.used_quantum += 1
-            cpu.current_burst -= 1
-            # add case for I/O time instead
-        
-        if io_queue:
-            for process in io_queue:
-                process.current_io += 1
-                if process.current_io == process.io_time[0]:
-                    io_queue.remove(process)
-                    process.current_io = 0
-                    process.io_time.pop(0)
-                    # add process back to ready queue
-                    ready_queue.append(process)
 
-        # testing
-        if time == 10:
-            break
+    print("SIMULATION DONE\n")
+
+    # [X] Compute and output turn-around time for each process
+    # [X] Compute and output average turn-around time
+    # [X] Compute waiting time for each process
+
+    turnaround_times: list[int] = []
+
+    sorted(completed, key=lambda Process: Process.name)
+
+    # Compute and output turn-around time for each process
+    for process in completed:
+        turnaround_time = process.completion_time - process.arrival_time
+        turnaround_times.append(turnaround_time)
+        print(f"Turn-around time for Process {process.name} : {process.completion_time} - {process.arrival_time} = {turnaround_time} ms")
     
-    # print final results
-    # turn around time
-    # waiting time
+    # Compute and output average turn-around time
+    print(f"Average Turn-around time = {sum(turnaround_times)/num_processes} ms")
 
-
-# Sample given in specs
-# A = Process(0, [5, 5, 5], [2, 2])
-# B = Process(2, [2, 6], [2])
-# C = Process(0, [30], [])
+    # Compute waiting time for each process
+    for process in completed: print(f"Waiting time for Process A : {process.completion_time - process.arrival_time - process.total_burst} ms")
 
 def main():
-    queues, processes, context_switch = get_input()
-    mlfq(queues, processes, context_switch)
+    queues, processes, num_processes, context_switch = get_input()
+    mlfq(queues, processes, num_processes, context_switch)
 
 if __name__ == "__main__":
     main()
